545b17252854f812267bad459fbef61d054f07d7
v5jay <vijay.atreyapurapu@utah.edu>
2016-05-24 04:00:57 +0000

    fix 5

--- site.rb
+++ site.rb
@@ -1,8 +1,12 @@
 require 'bundler/setup'
 require 'sinatra'
+
+set :port, 4000
+set :bind, '0.0.0.0'
+
 ENV['PORT'] ||= '4000'
- set :port, ENV['PORT']
- set :bind, '0.0.0.0'
+set :port, ENV['PORT']
+
 
  get '/' do
    erb :index
